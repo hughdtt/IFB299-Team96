960de80404db71833ae6f74e21f729a78fa8ef20
HUGHPC\Marten <hugh.dtt@hotmail.com>
2018-09-01 17:30:57 +1000

    Made reservations app, added changes to Projects/settings.py

--- Project/Project/urls.py
+++ Project/Project/urls.py
@@ -18,5 +18,6 @@ from django.urls import include,path
 
 urlpatterns = [
     path('login/', include('login.urls')),
+    path('reservation/', include('reservation.urls')),
     path('admin/', admin.site.urls),
 ]
