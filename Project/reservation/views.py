948a46d7d1d9edbd2970f5898c42884eea95bfde
HUGHPC\Marten <hugh.dtt@hotmail.com>
2018-09-02 12:09:00 +1000

    Added layout templates for Reservations, Added Django admin account, Added reservations model (for date and time stamps) and migrated, SHOULD change database to MySQL

--- Project/reservation/views.py
+++ Project/reservation/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<h2>Reservations page looking good</h2>")
+    #return HttpResponse("<h2>Reservations page looking good</h2>")
+    return render(request, 'reservation/index.html', {'title': 'Reservation1'})
 
