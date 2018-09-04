8615481a5b9da8701cc873407afec18ec76650dd
Alexander <teatime_dev@outlook.com>
2018-09-04 22:07:32 +1000

    database link

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+	'NAME' : 'car_rental',
+	'USER' : 'root',
+	'PASSWORD' :'MyNewPass',
+	'HOST' : 'localhost',
+	'PORT' : '3306',
     }
 }
 
@@ -107,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Australia/Brisbane'
 
 USE_I18N = True
 
@@ -120,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
