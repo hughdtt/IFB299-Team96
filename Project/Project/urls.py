d6c2c4187deb23adff47c53a4a8002fc9855d1ea
n9970509 <jaari.rey@gmail.com>
2018-09-04 14:20:24 +1000

    added searchpage with nothing in it

--- Project/Project/urls.py
+++ Project/Project/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import include,path
 
 urlpatterns = [
+
     path('login/', include('login.urls')),
     path('reservation/', include('reservation.urls')),
+    path('searchpage/', include('searchpage.urls')),
     path('admin/', admin.site.urls),
+    
 ]
