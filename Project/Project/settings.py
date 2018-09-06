878b50d54c4188008967d1a0a679ed631de20fbc
n9970509 <jaari.rey@gmail.com>
2018-09-06 21:06:41 +1000

    hope this works

--- Project/Project/settings.py
+++ Project/Project/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'login',
     'reservation',
+    'searchpage',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
