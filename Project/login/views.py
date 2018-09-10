afc5a6bf3ead5e7d4f4eeff91b198d5fffbd85fb
Alexander <teatime_dev@outlook.com>
2018-09-10 21:15:07 +1000

    login almost working

--- Project/login/views.py
+++ Project/login/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
 
 
-def index(request):
-    return HttpResponse("<h1>This is the login page. More info soon.")
+#def index(request):
+#    return HttpResponse("<h1>This is the login page. More info soon.")
